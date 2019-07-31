4261e814e4a04dbb4a4fc493db13c64733657bdd
Keenwarrior <anujgargcse@gmail.com>
2019-07-31 19:40:23 +0530

    session 3

---
+++ project/welcome.py
@@ -0,0 +1,6 @@
+print("It is a pycharm")
+
+import sample as chacha
+
+print(chacha.user)
+print(chacha.pwd)
---
+++ session-3/sample.py
@@ -0,0 +1,7 @@
+happy = 45
+
+sad = 20
+
+def looper():
+    for i in range(10):
+        print("Hello")
